833d1996a7327de7c2b34d2312b064cc0c487bf1
Kapil <kingranikapil3@gmail.com>
2024-02-18 21:33:54 +0530

    Feat: Added Helpers Which Covers CRUD Operations Of SQL

--- app/init.php
+++ app/init.php
@@ -1,10 +1,15 @@
 <?php
-
 $app = __DIR__;
 require_once("{$app}/../vendor/autoload.php");
 
+require_once("{$app}/helper/functions.inc.php");
+
 require_once("{$app}/classes/AppConfig.class.php");
 require_once("{$app}/classes/DatabaseConnection.class.php");
+require_once("{$app}/classes/QueryBuilder.class.php");
+require_once("{$app}/classes/User.class.php");
 
 $config = AppConfig::getInstance();
 $connection = new DatabaseConnection($config);
+
+User::migrate($connection, $config->APP_DEBUG);
--- dummy.php
+++ dummy.php
@@ -1,2 +1,32 @@
 <?php
 require_once("./app/init.php");
+
+$queryBuilder = new QueryBuilder($connection, $config->APP_DEBUG);
+
+// $userData = ['username' => 'pratik', 'email' => 'pp@gmail.com', 'password' => 'asd1234'];
+// $queryBuilder->table('users')
+//     ->insert($userData);
+
+// dd($queryBuilder->table('users')
+//     ->where('username', 'LIKE', 'P%')
+//     ->update(['email' => 'example@smaple.com', 'password' => '2345']));
+
+// dd($queryBuilder->table('users')
+//     ->where('username', 'LIKE', 'P%')
+//     ->get());
+
+// dd($queryBuilder->table('users')
+//     ->count());
+
+// dd($queryBuilder->table('users')
+//     ->select()
+//     ->get());
+
+// dd($queryBuilder->table('users')
+//     ->where('username', 'LIKE', 'sahil')
+//     ->delete());
+
+// dd($queryBuilder->table('users')
+//     ->limit(0, 2)
+//     ->select()
+//     ->get());
